436e0cd84d23e6f4667e187c4bda60c67f3c20fe
intoalmiala <into.almiala@gmail.com>
2022-12-08 10:18:00 +0200

    clean up 8.py

--- 2022/8.py
+++ 2022/8.py
@@ -2,13 +2,16 @@ from aocd import lines
 import numpy as np
 from itertools import starmap, product
 
+
 grid = np.array([[*map(int, line)] for line in lines])
 
+
 def views(y, x):
-    return (grid[:y,x][::-1],
+    return (grid[y-1:-1,x][::-1],
             grid[y+1:,x],
             grid[y,:x][::-1], 
             grid[y,x+1:])
+
 
 def visible(y, x):
     def dmax(xs):
@@ -16,9 +19,11 @@ def visible(y, x):
 
     return grid[y,x] > min(map(dmax, views(y, x)))
 
+
 coords = [*product(range(len(grid)), range(len(grid[0])))]
 
 print(sum(starmap(visible, coords)))
+
 
 def score(y, x):
     return np.prod([
