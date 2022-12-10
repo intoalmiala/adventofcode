d890bc88817ec0ff6053f823f76a21ff56955e9a
intoalmiala <into.almiala@gmail.com>
2022-12-10 12:48:20 +0200

    clean up 09.py even further

--- 2022/09.py
+++ 2022/09.py
@@ -4,26 +4,22 @@ import numpy as np
 
 dirs = list(map(np.array, [(0, -1), (1, 0), (-1, 0), (0, 1)]))
 dirmap = dict(zip("LDUR", dirs))
-
 motions = [(dirmap[d], int(c)) for d, c in re.findall(r"(\w) (\d+)", data)]
 
-def count_visited(N):
+def count_visited(N, tails):
     ps = np.zeros((N,2), dtype=int)
-
-    def near(p, q):
-        return abs(p-q).max() <= 1
-
-    visited = {tuple(ps[-1])}
+    visited = {t: {tuple(ps[t-1])} for t in tails}
 
     for dp, c in motions:
         for _ in range(c):
             ps[0] += dp
             for i in range(1, N):
-                if near(ps[i-1], ps[i]):
+                if abs(ps[i-1] - ps[i]).max() <= 1:
                     continue
                 ps[i] += np.sign(ps[i-1] - ps[i])
-            visited.add(tuple(ps[-1]))
-    return len(visited)
+            for t in tails:
+                visited[t].add(tuple(ps[t-1]))
 
-print(count_visited(2))
-print(count_visited(10))
+    return *map(len, visited.values()),
+
+print(*count_visited(10, [2, 10]))
