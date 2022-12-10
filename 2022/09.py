aeba00ba0dd8dc84ca219ca9d4fb147204f9624b
intoalmiala <into.almiala@gmail.com>
2022-12-10 12:42:01 +0200

    clean up 09.py

--- 2022/09.py
+++ 2022/09.py
@@ -4,7 +4,6 @@ import numpy as np
 
 dirs = list(map(np.array, [(0, -1), (1, 0), (-1, 0), (0, 1)]))
 dirmap = dict(zip("LDUR", dirs))
-diags = list(map(np.array, [(-1, -1), (-1, 1), (1, -1), (1, 1)]))
 
 motions = [(dirmap[d], int(c)) for d, c in re.findall(r"(\w) (\d+)", data)]
 
@@ -20,14 +19,9 @@ def count_visited(N):
         for _ in range(c):
             ps[0] += dp
             for i in range(1, N):
-                if near(ps[i-1], ps[i]): continue
-                if any(ps[i-1] == ps[i]):
-                    ps[i] = ps[i-1] - np.sign(ps[i-1]-ps[i])
+                if near(ps[i-1], ps[i]):
                     continue
-                for dd in diags:
-                    if near(ps[i-1], (new_p := ps[i]+dd)):
-                        ps[i] = new_p
-                        break
+                ps[i] += np.sign(ps[i-1] - ps[i])
             visited.add(tuple(ps[-1]))
     return len(visited)
 
