7177e316042ef6d0a7d44c90c97004eece997277
intoalmiala <into.almiala@gmail.com>
2022-12-11 19:59:54 +0200

    clean up 04.py formatting

--- 2022/04.py
+++ 2022/04.py
@@ -1,8 +1,14 @@
 from aocd import lines
 import re
 from itertools import starmap
+
 pairs = [tuple(map(int, re.findall(r"\d+", line))) for line in lines]
-contained = lambda a, b, c, d: a <= c and d <= b or c <= a and b <= d
-overlaps  = lambda a, b, c, d: c <= a <= d or a <= c <= b
+
+def contained(a, b, c, d):
+    return a <= c and d <= b or c <= a and b <= d
+
+def overlaps(a, b, c, d):
+    return c <= a <= d or a <= c <= b
+
 print(sum(starmap(contained, pairs)))
 print(sum(starmap(overlaps, pairs)))
