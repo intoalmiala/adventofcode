2178cfb167e62fe11311f4b39e8f1496aca4c2d6
intoalmiala <into.almiala@gmail.com>
2022-12-12 20:41:04 +0200

    replace numpy with vanilla features due to lack of satisfactory syntax sugar

--- 2022/12.py
+++ 2022/12.py
@@ -1,33 +1,27 @@
 from aocd import lines
-import numpy as np
 
-grid = np.array([[*line] for line in lines])
-n, m = grid.shape
+grid = {(y, x): c for y, line in enumerate(lines) for x, c in enumerate(line)}
+n, m = len(grid), len(grid[0,0])
 
-def findall(c):
-    return [*zip(*np.where(grid == c))]
-
-goal,  = findall('E')
-start, = findall('S')
+goal,  = (p for p in grid if grid[p] == 'E')
+start, = (p for p in grid if grid[p] == 'S')
 grid[start] = 'a'
 grid[goal]  = 'z'
 
-dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 def neighbours(y, x):
-    for (dy, dx) in dirs:
-        if 0 <= y+dy < n and 0 <= x+dx < m:
-            yield (y+dy, x+dx)
+    yield (y-1, x)
+    yield (y+1, x)
+    yield (y, x-1)
+    yield (y, x+1)
 
-queue = [goal]
-d = np.zeros((n, m), dtype=int)
-d[goal] = 1
+queue, dist = [goal], {goal: 0}
 
 for p in iter(queue):
     for q in neighbours(*p):
-        if d[q] or ord(grid[p]) > ord(grid[q]) + 1:
+        if q not in grid or q in dist or ord(grid[p]) > ord(grid[q]) + 1:
             continue
-        d[q] = d[p] + 1
+        dist[q] = dist[p] + 1
         queue.append(q)
 
-print(d[start]-1)
-print(min(filter(None, [d[p] for p in findall('a')])) - 1)
+print(dist[start])
+print(min(dist[p] for p in dist if grid[p] == 'a'))
