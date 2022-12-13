37a1dcbebd49074b413726af8f2221368a99615e
intoalmiala <into.almiala@gmail.com>
2022-12-13 17:57:55 +0200

    golf 12.py a bit

--- 2022/12.py
+++ 2022/12.py
@@ -4,8 +4,8 @@ grid = {(y, x): c for y, line in enumerate(lines) for x, c in enumerate(line)}
 
 goal,  = (p for p in grid if grid[p] == 'E')
 start, = (p for p in grid if grid[p] == 'S')
-grid[start] = 'a'
-grid[goal]  = 'z'
+
+grid[start], grid[goal] = 'a', 'z'
 
 def neighbours(y, x):
     yield (y-1, x)
@@ -17,10 +17,9 @@ queue, dist = [goal], {goal: 0}
 
 for p in iter(queue):
     for q in neighbours(*p):
-        if q not in grid or q in dist or ord(grid[p]) > ord(grid[q]) + 1:
-            continue
-        dist[q] = dist[p] + 1
-        queue.append(q)
+        if q in grid and q not in dist and ord(grid[p]) <= ord(grid[q]) + 1:
+            dist[q] = dist[p] + 1
+            queue.append(q)
 
 print(dist[start])
 print(min(dist[p] for p in dist if grid[p] == 'a'))
